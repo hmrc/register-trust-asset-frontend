2766f31a4b3030f169a6ff431f3068e91c50efbc
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-06-24 15:38:41 +0100

    IndexValidation fixed.

--- test/controllers/IndexValidation.scala
+++ test/controllers/IndexValidation.scala
@@ -14,22 +14,6 @@
  * limitations under the License.
  */
 
-/*
- * Copyright 2019 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package controllers
 
 import base.SpecBase
@@ -47,10 +31,10 @@ import views.html.ErrorTemplate
 trait IndexValidation extends SpecBase with ScalaCheckPropertyChecks with MockitoSugar with Generators {
 
   def validateIndex[A, B](
-                                generator: Gen[A],
-                                createPage: Int => QuestionPage[A],
-                                requestForIndex: Int => Request[B]
-                              )(implicit writes: Writes[A], writeable: Writeable[B]): Unit = {
+                           generator: Gen[A],
+                           createPage: Int => QuestionPage[A],
+                           requestForIndex: Int => Request[B]
+                         )(implicit writes: Writes[A], writeable: Writeable[B]): Unit = {
 
     "return not found if a given index is out of bounds" in {
       implicit val generatorDrivenConfig: PropertyCheckConfiguration = PropertyCheckConfiguration(
@@ -73,11 +57,12 @@ trait IndexValidation extends SpecBase with ScalaCheckPropertyChecks with Mockit
               uA.set(createPage(i), answer).success.value
           }
 
-          val application =
-            applicationBuilder(Some(userAnswers))
-              .build()
+          val application = applicationBuilder(Some(userAnswers))
+            .build()
 
-          val result = route(application, requestForIndex(index)).value
+          val request = requestForIndex(index)
+
+          val result = route(application, request).value
 
           val view = application.injector.instanceOf[ErrorTemplate]
 
@@ -85,7 +70,7 @@ trait IndexValidation extends SpecBase with ScalaCheckPropertyChecks with Mockit
             messages("global.error.pageNotFound404.title"),
             messages("global.error.pageNotFound404.heading"),
             messages("global.error.pageNotFound404.message")
-          )(fakeRequest, messages)
+          )(request, messages)
 
           status(result) mustEqual NOT_FOUND
 
