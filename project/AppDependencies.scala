c40bec7b00a3f632b9a7b12f08ee86f4e0459b10
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2023-08-18 10:47:36 +0100

    DDCE-4530: Changes as part of sm2 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,26 +3,26 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val bootstrapVersion = "7.15.0"
+  val bootstrapVersion = "7.21.0"
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc" %% "play-frontend-hmrc"            % "6.3.0-play-28",
-    "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.12.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"            % "6.8.0-play-28",
+    "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28"    % bootstrapVersion
   )
 
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % bootstrapVersion,
-    "org.scalatest"          %% "scalatest"                % "3.2.15",
+    "org.scalatest"          %% "scalatest"                % "3.2.16",
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0",
     "org.scalatestplus"      %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
-    "org.jsoup"               % "jsoup"                    % "1.15.4",
+    "org.jsoup"               % "jsoup"                    % "1.16.1",
     "com.typesafe.play"      %% "play-test"                % PlayVersion.current,
     "org.mockito"            %% "mockito-scala-scalatest"  % "1.17.14",
     "com.github.tomakehurst"  % "wiremock-standalone"      % "2.27.2",
     "wolfendale"             %% "scalacheck-gen-regexp"    % "0.1.2",
-    "com.vladsch.flexmark"    % "flexmark-all"             % "0.64.0"
+    "com.vladsch.flexmark"    % "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID]      = compile ++ test
