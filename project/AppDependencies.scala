537702f15ba9bbd4a95a1487d24347d9b81cf8a7
Adam George <151747054+adageorg@users.noreply.github.com>
2024-02-26 12:49:37 +0000

    DDCE-5379: Dependency updates for Tudor crown change.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,18 +5,18 @@ object AppDependencies {
   val bootstrapVersion = "7.23.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "play-frontend-hmrc"            % "7.29.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-28"    % "8.5.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28"    % bootstrapVersion
   )
 
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"         %% "bootstrap-test-play-28"  % bootstrapVersion,
-    "org.scalatest"       %% "scalatest"               % "3.2.17",
-    "org.scalatestplus"   %% "scalacheck-1-17"         % "3.2.17.0",
+    "org.scalatest"       %% "scalatest"               % "3.2.18",
+    "org.scalatestplus"   %% "scalacheck-1-17"         % "3.2.18.0",
     "org.jsoup"            % "jsoup"                   % "1.17.2",
     "org.mockito"         %% "mockito-scala-scalatest" % "1.17.30",
-    "org.wiremock"         % "wiremock-standalone"     % "3.3.1",
+    "org.wiremock"         % "wiremock-standalone"     % "3.4.2",
     "wolfendale"          %% "scalacheck-gen-regexp"   % "0.1.2",
     "com.vladsch.flexmark" % "flexmark-all"            % "0.64.8"
   ).map(_ % Test)
