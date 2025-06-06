01eb8d4a1c30788c5f3ea4b9411cbd0ac2707cca
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-06-06 08:26:59 +0100

    DDCE-3621 - Update References to "Register and Maintain a Trust" across the Service

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   val bootstrapVersion = "9.13.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.3.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.1.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % bootstrapVersion
   )
