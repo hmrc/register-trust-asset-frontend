3361d103855809f3d0bd52319dd59796b99890fd
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-11-05 11:50:28 +0000

    DDCE-7509 - updating coverage

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -5,7 +5,7 @@ object CodeCoverageSettings {
 
   private val settings: Seq[Setting[?]] = Seq(
     coverageExcludedPackages := "<empty>;Reverse.*;..*Routes.*;",
-    coverageMinimumStmtTotal := 80,
+    coverageMinimumStmtTotal := 85,
     coverageFailOnMinimum := true,
     coverageHighlighting := true
   )
