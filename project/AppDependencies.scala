5a7a0b2c0058da59922b348e5ddc7d1541a731fb
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-11-11 14:03:38 +0000

    DDCE-7509 - bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,10 +2,10 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.3.0"
+  val bootstrapVersion = "10.4.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.19.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.20.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % bootstrapVersion
   )
