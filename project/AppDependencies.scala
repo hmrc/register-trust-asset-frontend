a923cc9b33c7a72131bd9141b5bc77d0fabbbd8d
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2025-12-02 13:59:33 +0000

    DDCE-7639 bump play-frontend-hmrc

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   val bootstrapVersion = "10.4.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.20.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.21.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % bootstrapVersion
   )
