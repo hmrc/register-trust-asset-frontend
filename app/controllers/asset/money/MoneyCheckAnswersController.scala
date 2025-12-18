ed614d3e2f90a7bcac37232d5940c18d750be7c1
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2025-12-18 13:22:54 +0000

    DDCE-6594 change from plural to singular for information-you-need non… (#146)

--- app/controllers/asset/money/MoneyCheckAnswersController.scala
+++ app/controllers/asset/money/MoneyCheckAnswersController.scala
@@ -25,7 +25,7 @@ import utils.print.MoneyPrintHelper
 import views.html.asset.money.MoneyAnswersView
 
 import javax.inject.Inject
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
 class MoneyCheckAnswersController @Inject() (
   override val messagesApi: MessagesApi,
