958f1eeeb6c5a0009f9a54db70dc984f19de1d51
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2026-01-23 13:23:10 +0000

    DDCE-7774 Migrate to new Service Navigation Component

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   val bootstrapVersion = "10.5.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.25.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.28.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.4.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % bootstrapVersion
   )
